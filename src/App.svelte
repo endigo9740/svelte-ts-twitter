77ce0fe22d8ea26d88af97eabd2f8323981d7524
Chris <gundamx9740@gmail.com>
2021-09-14 18:49:02 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Tweets from "./lib/Tweets.svelte";
+</script>
+
+<main>
+    <Tweets />
+</main>
+
+<style>
+    :root {font-family: sans-serif;}
+    main {max-width: 1200px; margin: 20px auto;}
+</style>
